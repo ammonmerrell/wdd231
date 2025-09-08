c9729efd629a5556c0cf95202a8f2a50abbf6ca6
ammonmerrell <amerrell@byupathway.edu>
2025-09-08 16:45:07 -0600

    filled out companies

---
+++ chamber/scripts/companies.js
@@ -0,0 +1,27 @@
+const url = 'https://github.com/ammonmerrell/wdd231/blob/main/chamber/data/members.json';
+const cards = document.querySelector("#companies");
+async function getCompanyData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCompanies(data.companies);
+}
+
+getCompanyData();
+
+const displayCompanies = (companies) => {
+    companies.forEach((company) => {
+        let card = document.createElement("section");
+        let aname = document.createElement("p");
+        let pnum = document.createElement("p");
+        let alink = document.createElement("p");
+
+        aname.textContent = `${company.name} ${company.address}`;
+        pnum.textContent = `${company.phonenumber}`;
+        alink.textContent = `${company.website}`;
+
+        card.appendChild(aname);
+        card.appendChild(pnum);
+        card.appendChild(alink);
+        cards.appendChild(card);
+    })
+}
