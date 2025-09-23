7dfe1a877f27ba12bf9eb825811a5d312372a1e1
ammonmerrell <amerrell@byupathway.edu>
2025-09-23 09:51:26 -0600

    updated weather to show picture and discripton

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,7 +1,7 @@
 const weatherTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDest = document.querySelector('#figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.76&lon=-111.89&appid=6c343c2a3ace250294bb79f581a2a9df';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.76&lon=-111.89&units=imperial&appid=6c343c2a3ace250294bb79f581a2a9df';
 
 
 
@@ -24,11 +24,11 @@ function displayResults(data) {
 
     let desc = data.weather[0].icon;
     weatherTemp.innerHTML = `${data.main.temp}&deg;C`;
-    const iconsrc = `https//openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', data.weather[0].description);
-    captionDest.textContent = `${data.weather.desc}`;
+    captionDest.textContent = `${data.weather[0].description}`;
     console.log(weatherIcon);
 }
 apiFetch();
