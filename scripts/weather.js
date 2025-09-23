390996841a50c9d6222d6f892e0b0789972f1dcd
ammonmerrell <amerrell@byupathway.edu>
2025-09-23 09:28:03 -0600

    changed mesurement to farinhiet

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,7 @@
 const weatherTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDest = document.querySelector('#figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=6c343c2a3ace250294bb79f581a2a9df';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=6c343c2a3ace250294bb79f581a2a9df';
 
 
 
