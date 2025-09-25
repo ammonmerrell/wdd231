680165cab8b865f7759702f4a29c2818891189c0
ammonmerrell <amerrell@byupathway.edu>
2025-09-25 12:18:39 -0600

    filled out and added week3(04) code for forms

---
+++ week03/startHere/js/scripts.js
@@ -0,0 +1,20 @@
+const getString = window.location.search;
+console.log(getString);
+
+const myInfo = new URLSearchParams(getString);
+console.log(myInfo)
+
+console.log(myInfo.get('first'));
+console.log(myInfo.get('last'));
+console.log(myInfo.get('phone'));
+console.log(myInfo.get('email'));
+console.log(myInfo.get('ordinance'));
+console.log(myInfo.get('date'));
+console.log(myInfo.get('location'));
+
+const results = document.querySelector('results');
+
+results.innerHTML = `
+<p>Appointment for ${myInfo.get('first')} ${myInfo.get('last')}</p>
+<p>proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} temple.</p>
+<p>your phone number: ${myInfo.get('phone')} \nemail:${myInfo.get('email') }</p>`;
