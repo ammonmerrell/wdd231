e7fb05b23a80d7527799f8383e0606beb0dd7ede
ammonmerrell <amerrell@byupathway.edu>
2025-09-24 21:23:41 -0600

    filled out module for js

--- scripts/module.js
+++ scripts/module.js
@@ -1,1 +1,10 @@
 const modal = document.getElementById('course-info');
+const endModal = document.getElementById('end-info');
+const modalButton = document.getElementById('button');
+
+modalButton.addEventListener('click', () => {
+    modal.showModal()
+});
+endModal.addEventListener('click', () => {
+    modal.close()
+})
