89343dfed0ce5fea6fdf5446aeb30adf54969d0e
ammonmerrell <amerrell@byupathway.edu>
2025-10-02 10:07:31 -0600

    finished the local storage date portion.

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,15 +1,20 @@
 const visitDisplay = document.querySelector(".visited");
-const lastVisit = new Date();
-let visits = Number(window.localStorage.getItem("visits-ls"));
-if (visits == 0) {
+let visits = Date.now();
+let lastVisit = Number(window.localStorage.getItem("lastVisit-ls"));
+let daysFromVisit = (visits - lastVisit) / 86400000
+console.log(daysFromVisit)
+if (lastVisit == "") {
     visitDisplay.textContent = ` Welcome! Let us know if you have any questions.`;
-    visits++;
-} else if(visits <= 2 ) {
+    lastVisit = visits;
+} else if(daysFromVisit < 1) {
     visitDisplay.textContent = ` Back so soon! Awesome!`;
-    visits++;
+    lastVisit = visits;
+} else if (daysFromVisit < 2){
+    visitDisplay.textContent = ` You last visited ${Math.round(daysFromVisit)} day ago.`
+    lastVisit = visits;
 } else {
-    visitDisplay.textContent = ` You last visited ${visits} days ago.`
-    visits++;
+    visitDisplay.textContent = ` You last visited ${Math.round(daysFromVisit)} days ago.`
+    lastVisit = visits;
 }
 
-localStorage.setItem("visits-ls", visits);
+localStorage.setItem("lastVisit-ls", lastVisit);
