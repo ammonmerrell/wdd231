cfbc39ec519607eb31cca7b0fd315a67643dcd63
ammonmerrell <amerrell@byupathway.edu>
2025-10-09 22:31:16 -0600

    made year code  and modified code

--- final/scripts/footer.js
+++ final/scripts/footer.js
@@ -1,11 +1,9 @@
-// const curYear = document.querySelector('#year');
+const curYear = document.querySelector('#year');
 
-// const now = new Date();
+const now = new Date();
 
-// curYear.innerHTML = `<span class="highlight">${now.getFullYear()}</span> `;
+curYear.innerHTML = `<span class="highlight">${now.getFullYear()}</span> `;
 
-const year = document.querySelector('#year');
-
-const today = new Date();
-
-year.innerHTML = `<span class="highlight">${today.getFullYear()}</span> `;
+const mod = document.querySelector('#modified');
+let lastM = document.lastModified;
+mod.innerHTML = `<span class="highlight">${lastM}</span> `
