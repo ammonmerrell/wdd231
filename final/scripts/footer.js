543544f722d31ccbb957253673e77542a1ae0001
ammonmerrell <amerrell@byupathway.edu>
2025-10-09 22:27:24 -0600

    added footer.js and link in index.html

---
+++ final/scripts/footer.js
@@ -0,0 +1,11 @@
+// const curYear = document.querySelector('#year');
+
+// const now = new Date();
+
+// curYear.innerHTML = `<span class="highlight">${now.getFullYear()}</span> `;
+
+const year = document.querySelector('#year');
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span> `;
