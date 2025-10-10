c2c3931ef2a48d7ba0f68600debaae484ea18df7
ammonmerrell <amerrell@byupathway.edu>
2025-10-10 11:21:51 -0600

    created civilizations.js module in data

---
+++ final/data/civilizations.js
@@ -0,0 +1,102 @@
+export const civs = [
+    {
+        "name": "Aksum",
+        "ability": "+2 gold on resources",
+        "uniDev": "Hawilt; +2 gold on inprovment",
+        "civUnit": "Tankwa; trade ship with Increased range immune to pillaging.",
+        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water."
+    },
+    {
+        "name": "Rome",
+        "ability": "+1 culture on districts in capital, and city centers in towns",
+        "uniDev": "Forum(Temple of Jupiter + Basilica); +1 gold and culture for every roman tration in government.",
+        "civUnit": "Legatus; army commander who can create town every three promotions.",
+        "milUnit": "Legion; gains +1 combat for every tradition in the governmant"
+    },
+    {
+        "name": "Han",
+        "ability": "Extra growth in settlements after first growth event",
+        "uniDev": "Han Great Wall; +2 culture on inprovment, Fortification, +1 happiness on adjacent Great Wall",
+        "civUnit": "Shì Dàfū; great person who can only be trained in capitol with 10 population, effect is random.",
+        "milUnit": "Chu-Ko-Nu; ranged unit with increased combat when attack adjacent units, increased stats."
+    },
+    {
+        "name": "Greece",
+        "ability": "+3 influence on palace",
+        "uniDev": "Acropolis(Parthenon + Odeon Building); +2 gold for each city-state befriended",
+        "civUnit": "Logios; great person who can only be trained in cities with an Acropolis(unique quarter), effect is random.",
+        "milUnit": "Hoplite; +2 combat if adjacent to another Hoplite."
+    }, {
+        "name": "Egypt",
+        "ability": "+1 production on Navigable Rivers",
+        "uniDev": "Necropolis(Mastaba + Mortuary Temple); gain +100 gold when a wonder is completed in this city",
+        "civUnit": "Tjaty; great person who can only be trained in cities with a Necropolis(unique quarter), effect is random.",
+        "milUnit": "Medjay; +3 combat in friendly territory, doubled when on your buildings you own."
+    },
+    {
+        "name": "",
+        "ability": "+1 culture on districts in capital, and city centers in towns",
+        "uniDev": "Forum(Temple of Jupiter + Basilica); +1 gold and culture for every roman tration in government.",
+        "civUnit": "Legatus; army commander who can create town every three promotions.",
+        "milUnit": "Legion; gains +1 combat for every tradition in the governmant"
+    }, {
+        "name": "",
+        "ability": "+2 gold on resources",
+        "uniDev": "Hawilt; +2 gold on inprovment",
+        "civUnit": "Tankwa; trade ship with Increased range immune to pillaging.",
+        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water."
+    },
+    {
+        "name": "",
+        "ability": "+1 culture on districts in capital, and city centers in towns",
+        "uniDev": "Forum(Temple of Jupiter + Basilica); +1 gold and culture for every roman tration in government.",
+        "civUnit": "Legatus; army commander who can create town every three promotions.",
+        "milUnit": "Legion; gains +1 combat for every tradition in the governmant"
+    }, {
+        "name": "",
+        "ability": "+2 gold on resources",
+        "uniDev": "Hawilt; +2 gold on inprovment",
+        "civUnit": "Tankwa; trade ship with Increased range immune to pillaging.",
+        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water."
+    },
+    {
+        "name": "",
+        "ability": "+1 culture on districts in capital, and city centers in towns",
+        "uniDev": "Forum(Temple of Jupiter + Basilica); +1 gold and culture for every roman tration in government.",
+        "civUnit": "Legatus; army commander who can create town every three promotions.",
+        "milUnit": "Legion; gains +1 combat for every tradition in the governmant"
+    }, {
+        "name": "Aksum",
+        "ability": "+2 gold on resources",
+        "uniDev": "Hawilt; +2 gold on inprovment",
+        "civUnit": "Tankwa; trade ship with Increased range immune to pillaging.",
+        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water."
+    },
+    {
+        "name": "Rome",
+        "ability": "+1 culture on districts in capital, and city centers in towns",
+        "uniDev": "Forum(Temple of Jupiter + Basilica); +1 gold and culture for every roman tration in government.",
+        "civUnit": "Legatus; army commander who can create town every three promotions.",
+        "milUnit": "Legion; gains +1 combat for every tradition in the governmant"
+    }, {
+        "name": "Aksum",
+        "ability": "+2 gold on resources",
+        "uniDev": "Hawilt; +2 gold on inprovment",
+        "civUnit": "Tankwa; trade ship with Increased range immune to pillaging.",
+        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water."
+    },
+    {
+        "name": "Rome",
+        "ability": "+1 culture on districts in capital, and city centers in towns",
+        "uniDev": "Forum(Temple of Jupiter + Basilica); +1 gold and culture for every roman tration in government.",
+        "civUnit": "Legatus; army commander who can create town every three promotions.",
+        "milUnit": "Legion; gains +1 combat for every tradition in the governmant"
+    }, {
+        "name": "Aksum",
+        "ability": "+2 gold on resources",
+        "uniDev": "Hawilt; +2 gold on inprovment",
+        "civUnit": "Tankwa; trade ship with Increased range immune to pillaging.",
+        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water."
+    },
+    
+]
