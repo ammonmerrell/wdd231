8ce367e51fed54a0bdd91db32433f59da4f04785
ammonmerrell <amerrell@byupathway.edu>
2025-10-11 18:41:07 -0600

    added pictures to civilizations

--- final/data/civilizations.js
+++ final/data/civilizations.js
@@ -4,99 +4,114 @@ export const civs = [
         "ability": "+2 gold on resources",
         "uniDev": "Hawilt; +2 gold on inprovment",
         "civUnit": "Tankwa; trade ship with Increased range immune to pillaging.",
-        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water."
+        "milUnit": "Dhow; level 2 ship can make trade route and +4 combat in shallow water.",
+        "picture": "images/aksum-1920.jpg"
     },
     {
         "name": "Rome",
         "ability": "+1 culture on districts in capital, and city centers in towns",
         "uniDev": "Forum(Temple of Jupiter + Basilica); +1 gold and culture for every roman tration in government.",
         "civUnit": "Legatus; army commander who can create town every three promotions.",
-        "milUnit": "Legion; gains +1 combat for every tradition in the governmant"
+        "milUnit": "Legion; gains +1 combat for every tradition in the governmant",
+        "picture": "images/rome-1920.jpg"
     },
     {
         "name": "Han",
         "ability": "Extra growth in settlements after first growth event",
         "uniDev": "Han Great Wall; +2 culture on inprovment, Fortification, +1 happiness on adjacent Great Wall",
         "civUnit": "Shì Dàfū; great person who can only be trained in capitol with 10 population, effect is random.",
-        "milUnit": "Chu-Ko-Nu; ranged unit with increased combat when attack adjacent units, increased stats."
+        "milUnit": "Chu-Ko-Nu; ranged unit with increased combat when attack adjacent units, increased stats.",
+        "picture": "images/HAN_CHINA.jpg"
     },
     {
         "name": "Greece",
         "ability": "+3 influence on palace",
         "uniDev": "Acropolis(Parthenon + Odeon Building); +2 gold for each city-state befriended",
         "civUnit": "Logios; great person who can only be trained in cities with an Acropolis(unique quarter), effect is random.",
-        "milUnit": "Hoplite; +2 combat if adjacent to another Hoplite."
+        "milUnit": "Hoplite; +2 combat if adjacent to another Hoplite.",
+        "picture": "images/Greece.jpg"
     }, {
         "name": "Egypt",
         "ability": "+1 production on Navigable Rivers",
         "uniDev": "Necropolis(Mastaba + Mortuary Temple); gain +100 gold when a wonder is completed in this city",
         "civUnit": "Tjaty; great person who can only be trained in cities with a Necropolis(unique quarter), effect is random.",
-        "milUnit": "Medjay; +3 combat in friendly territory, doubled when on your buildings you own."
+        "milUnit": "Medjay; +3 combat in friendly territory, doubled when on your buildings you own.",
+        "picture": "images/egypt-1920.jpg"
     },
     {
         "name": "Spain",
         "ability": "%15 cheaper to convert town to city, %30 in distant lands",
         "uniDev": "Plaza(Casa Consistorial + Casa de Contratación); +2 gold in this city for every settlement in distant lands(can only be built in homelands)",
         "civUnit": "Conquistador; great person who can only be built in cities with a wharf, random effects.",
-        "milUnit": "Tercio; +2 combat, adjacent units get +3 combat vs cavalry units(bonus is only applied once)"
+        "milUnit": "Tercio; +2 combat, adjacent units get +3 combat vs cavalry units(bonus is only applied once)",
+        "picture": "images/CIV7_Spain.jpg"
     }, {
         "name": "Abbasid ",
         "ability": "gain +30 gold for each rural tile in city when it creates a specialist.",
         "uniDev": "Ulema(Madrasa + Mosque) +1 science to all specialists in city.",
         "civUnit": "Ālim; great person who can only be trained in cities with a Ulema(unique quarter), random effects.",
-        "milUnit": "Mamluk; -10 combat, +1 combat for each building in city when standing on a building."
+        "milUnit": "Mamluk; -10 combat, +1 combat for each building in city when standing on a building.",
+        "picture": "images/Copy_of_Exploration-Civ_Abbasid_01.jpg"
     },
     {
         "name": "Songhai ",
         "ability": "+2 resource capacity in cities on navigable rivers, trade ships cannot be plundered on navigable rivers.",
         "uniDev": "Caravanserai; +6 gold, +1 gold for each adjacent river and resource.",
         "civUnit": "Tajiro; unique merchent, can gain +100 gold when making a trade route with a navigable river.",
-        "milUnit": "Gold Bangles Infantry; +5 combat when on a resource tile, +%100 gold when pillaging a trade route."
+        "milUnit": "Gold Bangles Infantry; +5 combat when on a resource tile, +%100 gold when pillaging a trade route.",
+        "picture": "images/Civ7_Songhai.jpg"
     }, {
         "name": "Ming",
         "ability": "+50% science in capital, -15 science for each policy in government.",
         "uniDev": "Ming Great Wall; +5 culture, +1 gold for each adjacent Fortification, counts as a Fortification.",
         "civUnit": "Mandarin; unique merchent, gain +100 gold when using the 'Create a Road' action.",
-        "milUnit": "Xunleichong; has +5 combat in featureless terrain, has a ranged attack."
+        "milUnit": "Xunleichong; has +5 combat in featureless terrain, has a ranged attack.",
+        "picture": "images/Civ7_Ming.jpg"
     },
     {
         "name": "Norman",
         "ability": "+5 combat for land units when adjacent to coast, land units get +1 movement when embarked.",
         "uniDev": "Donjon(Motte + Bailey); get a free Chevaler(unique unit) when built, cavalry units are +10% cheaper in this city.",
         "civUnit": "Sokeman; unique settler, gain wall in city center when founding a town.",
-        "milUnit": "Chevaler; +3 combat vs units with less movement."
+        "milUnit": "Chevaler; +3 combat vs units with less movement.",
+        "picture": "images/Civ7_Norman.jpg"
     }, {
         "name": "America",
         "ability": "gain +100 gold when improving a resource.",
         "uniDev": "Industrial Park(Steel Mill + Railyard); +2 resource capacity in this city.",
         "civUnit": "Prospector; can claim and inprove unowned resource tiles within 5 tiles from settlement, creates a path back to settlement.",
-        "milUnit": "Marine; no combat or movement penalties from attacking from Embarkation(coast)."
+        "milUnit": "Marine; no combat or movement penalties from attacking from Embarkation(coast).",
+        "picture": "images/Civ7_America_1_.jpg"
     },
     {
         "name": "Mexico",
         "ability": "start with unique government type with one celebration effect: '+30% culture'. Cannot enter other government types.",
         "uniDev": "Zócalo(Catedral + Portal de Mercaderes); +2 culture for every tradion in government.",
         "civUnit": "Revolucionario; great person who can only be trained in cities with a Zócalo(unique quarter), random effects.",
-        "milUnit": "Soldaderas; adjacent units heal +10 hp, effect does not stack."
+        "milUnit": "Soldaderas; adjacent units heal +10 hp, effect does not stack.",
+        "picture": "images/Civ7_Mexico.jpg"
     }, {
         "name": "Qing",
         "ability": "+4 gold, +3 culture, +2 influence, and -1 science on imported resources.",
         "uniDev": "Huiguan(Shiguan + Qianzhuang); +25% influence in this city.",
         "civUnit": "Hangshang;  unique merchent, gain +50 gold for each resource from naval trade route.",
-        "milUnit": "Gusa; +4 combat when adjacent to another Gusa."
+        "milUnit": "Gusa; +4 combat when adjacent to another Gusa.",
+        "picture": "images/Civ7_Qing.jpg"
     },
     {
         "name": "Russia",
         "ability": "+1 culture and +1 science from districts, doubled in tundra.",
         "uniDev": "Obshchina; +2 culture if on tundra, +2 food on farms in this settlement.",
         "civUnit": "Cossack; unique bonus cavalry unit, -5 combat. +4 when in friendly territory, does not replace other cavalry units.",
-        "milUnit": "Katyusha Rocket Launcher; +1 movment, -10 siege combat, can damage units around target area."
+        "milUnit": "Katyusha Rocket Launcher; +1 movment, -10 siege combat, can damage units around target area.",
+        "picture": "images/Civ7_Russia.jpg"
     }, {
         "name": "Mughal",
         "ability": "+75% gold from everything, -25% everything else.",
         "uniDev": "Stepwell; +2 gold, +2 food from adjacent farms.",
         "civUnit": "Zamindar; gain +1 population when founding towns.",
-        "milUnit": "Sepoy; has siege(45) and range(5)."
+        "milUnit": "Sepoy; has siege(45) and range(5).",
+        "picture": "images/Civ7_Mughal.jpg"
     },
     
 ]
