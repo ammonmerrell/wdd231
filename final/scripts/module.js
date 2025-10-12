f3614dc7994d8590f2d757427d3aacc0e96f41e2
ammonmerrell <amerrell@byupathway.edu>
2025-10-11 21:35:54 -0600

    added modul and link

---
+++ final/scripts/module.js
@@ -0,0 +1,6 @@
+const mydialog = document.querySelector('#mydialog')
+const infor = document.querySelector('#info')
+const closeit = document.querySelector('#myclose')
+
+closeit.addEventListener('click', () => mydialog.close())
+
