5c66fff0ac76f556013d1a7bd0355cdebec11386
ammonmerrell <amerrell@byupathway.edu>
2025-10-11 20:12:36 -0600

    made visits and reference to that page

---
+++ final/scripts/visits.js
@@ -0,0 +1,20 @@
+const visitDisplay = document.querySelector(".visited");
+let visits = Date.now();
+let lastVisit = Number(window.localStorage.getItem("lastVisit-ls"));
+let daysFromVisit = (visits - lastVisit) / 86400000
+console.log(daysFromVisit)
+if (lastVisit == "") {
+    visitDisplay.textContent = ` Welcome! Let us know if you have any questions.`;
+    lastVisit = visits;
+} else if (daysFromVisit < 1) {
+    visitDisplay.textContent = ` Back so soon! Awesome!`;
+    lastVisit = visits;
+} else if (daysFromVisit < 2) {
+    visitDisplay.textContent = ` You last visited ${Math.round(daysFromVisit)} day ago.`
+    lastVisit = visits;
+} else {
+    visitDisplay.textContent = ` You last visited ${Math.round(daysFromVisit)} days ago.`
+    lastVisit = visits;
+}
+
+localStorage.setItem("lastVisit-ls", lastVisit);
