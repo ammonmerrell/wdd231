bf7c3483fa0dfa817f4a8730887ad99dc7b94d34
ammonmerrell <amerrell@byupathway.edu>
2025-10-11 20:50:18 -0600

    filled out result.js

--- final/scripts/result.js
+++ final/scripts/result.js
@@ -1,1 +1,5 @@
-const 
+const getString = window.location.search;
+
+const myGuide = new URLSearchParams(getString)
+
+results.innerHTML = `Thanks for your guide application under ${myGuide.get("user")}, with an email of "${myGuide.get("email")}" that talks about ${myGuide.get("desc")}. We Will publish it as soon as we can!`;
